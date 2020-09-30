164d1f4f3a4f4489c063050dc80c209fe37d83a4
salim <salim.nassor.sn@gmail.com>
2020-10-01 01:18:53 +0300

    (feature): Home page and project structure

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,44 @@
 import { Link } from "gatsby"
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import "./header.css"
 import PropTypes from "prop-types"
 import React from "react"
+import { Container } from "react-bootstrap"
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    <Navbar className="py-5" bg="transparent" expand="lg" fixed="top">
+    <Container>
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link as='li'>
+          <Link to="/">{siteTitle}</Link>      
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" as='li'>
+          <Link to="/page-2/">About</Link>
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2" as='li'>
+          <Link to="/">Partners</Link>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" as='li'>
+          <Link to="/">Contacts</Link>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      </Navbar.Collapse>
+    </Container>
+</Navbar>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "gatsby"
+import "../components/pages-style/home.css"
+import { Container } from "react-bootstrap"
+
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,9 +12,39 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <div
+    className="home carousel">
+     <Container className="h-100 w-100">
+     <div className="d-flex h-100 align-items-center">
+       <div className="d-block">
+       <h1 className="my-1">
+          Experienced.
+        </h1>
+        <h1 className="my-1 light-blue">
+        professional.
+        </h1>
+        <h1 className="my-1">
+        Purpose-drived.
+        </h1>
+       <div className="subtitle my-3">
+         <p className="pt-3">
+           We bring <b>Proven, unique blend of business, quantitative and <br /> and technological skills</b> to the industry
+         </p>
+       </div>
+       <div className="d-inline-block my-2">
+       <button type="button" class="btn btn-lg btn-outline-light text-white">
+         OUR SERVICES
+       </button>
+
+       <button type="button"  id="work_with_us" class="btn btn-light btn-lg primary-color mx-3">
+         WORK WITH US
+       </button>
+       </div>
+       </div>
+      </div>
+     </Container>
+
+    </div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
