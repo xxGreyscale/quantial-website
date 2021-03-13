3b035b8de3a21a23b7599ffda82b862936bf41e3
salim <salim.nassor.sn@gmail.com>
2021-03-13 05:45:20 +0300

    Feat: added credits in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,9 +11,9 @@ const Footer = () => {
             <footer style={{
             marginTop: `2rem`
           }}>
-              <Container className="h-100">
-              <div className="d-flex h-100 align-items-center">
-                  <div className="d-block">
+              <Container className="h-100 py-5">
+              <div className="d-flex h-100 align-items-center py-5">
+                  <div className="d-block py-5">
                     <h4 className="underline text-white m-0">
                         Quantial technology solutions
                     </h4>
@@ -25,20 +25,16 @@ const Footer = () => {
                   </div>
               </div>
               </Container>
-           <div className="bg-light py-3">
+           <div className="d-flex justify-contents-end bg-light py-3 ">
                <div className="container-lg w-100">
                    <div className="row font-weight-bold w-100" style={{fontSize: '0.725rem'}}>
                    <div className="col-12 col-md col-lg">
-                    © {new Date().getFullYear()}, Quantial Technology solutions.  All Rights Reserved.
+                    © {new Date().getFullYear()}, Quantial Technology solutions.  All Rights Reserved. 
+                   
                    </div>
-                   {/* <div className="col-12 col-md col-lg ml-lg-auto text-lg-right p-lg-0 mt-2 mt-md-0 mt-lg-0">
-                       <p className="d-inline mx-lg-4">
-                           Privacy Policy
-                       </p>
-                       <p className="d-inline ml-4">
-                           Terms of Service
-                       </p>
-                   </div> */}
+                   <div className="col-12 col-md col-lg ml-lg-auto text-right pr-lg-0">
+                        Designed and developed by <a href="mailto:madebysalinastic@gmail.com">madebysalinastic@gmail.com</a> 
+                   </div>
                    </div>
 
                </div>
